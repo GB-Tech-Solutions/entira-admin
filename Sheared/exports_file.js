b211e9e179e9a090589e222fe89aedebca17d273
ChitrakGit <chitrak17444@gmail.com>
2020-12-23 13:39:30 +0530

    mongo link changes

--- Sheared/exports_file.js
+++ Sheared/exports_file.js
@@ -1,8 +1,8 @@
 
 
 
-exports.MongoLink = "MongoURI=mongodb://localhost/EntiraClinic";
+//exports.MongoLink = "MongoURI=mongodb://localhost/EntiraClinic";
 
-//exports.MongoLink = "mongodb+srv://entira:admin%admin@cluster0.6qqfv.mongodb.net/EntiraClinic?retryWrites=true&w=majority"
+exports.MongoLink = "mongodb://adminUser:123dbADMIN123@entiratest-shard-00-00.ivyqp.mongodb.net:27017,entiratest-shard-00-01.ivyqp.mongodb.net:27017,entiratest-shard-00-02.ivyqp.mongodb.net:27017/entira?replicaSet=atlas-w4frhz-shard-0&ssl=true&authSource=admin"
 
 exports.processToken = "restricted";
