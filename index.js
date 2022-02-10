591348752859acb9b87e2ecf5933678351a1e1f8
chitrak biswas <chitrak1744@gmail.com>
2022-02-10 13:56:45 +0530

    https code added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const EventEmitter = require('events');
+// const https = require('https');
 require('dotenv').config();
 
 const {MongoLink} = require('./Sheared/exports_file');
@@ -40,5 +41,15 @@ app.use("/Login",require('./Routes/Login'))
 app.use("/appt",require('./Routes/Appointment'))
 
 
+const httpsServer = https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/my_api_url/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/my_api_url/fullchain.pem'),
+  }, app);
+
+
 const port = process.env.PORT || 5000;
 app.listen( port, () => console.log('Express server started at port : ' + port) );
+
+// httpsServer.listen(port, () => {
+//     console.log('HTTPS Server running on port 443');
+// });
