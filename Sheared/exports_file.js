08ec152dcebbd7a87b16f03b35451254fccaf316
ChitrakGit <chitrak1744@gmail.com>
2022-09-14 10:37:49 +0530

    dont marge

--- Sheared/exports_file.js
+++ Sheared/exports_file.js
@@ -1,7 +1,7 @@
 
 
 
-//exports.MongoLink = "MongoURI=mongodb://localhost/EntiraClinic";
+exports.MongoLink = "mongodb://localhost/EntiraClinic";
 
 exports.MongoLink = "mongodb://adminUser:123dbADMIN123@entiratest-shard-00-00.ivyqp.mongodb.net:27017,entiratest-shard-00-01.ivyqp.mongodb.net:27017,entiratest-shard-00-02.ivyqp.mongodb.net:27017/EntiraClinic?replicaSet=atlas-w4frhz-shard-0&ssl=true&authSource=admin"
 
